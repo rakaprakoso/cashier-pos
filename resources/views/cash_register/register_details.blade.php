9d8965640c0851a3b44784066a2a27f8b7f13e1c
yodifm <yodifm@gmail.com>
2022-09-04 01:47:30 +0700

    cetak 2 page

--- resources/views/cash_register/register_details.blade.php
+++ resources/views/cash_register/register_details.blade.php
@@ -221,7 +221,7 @@
     <div class="modal-footer">
       <button type="button" class="btn btn-primary no-print" 
         aria-label="Print" 
-          onclick="$(this).closest('div.modal').printThis();">
+          onclick="$(this).closest('div.modal-body').printThis();">
         <i class="fa fa-print"></i> @lang( 'messages.print' )
       </button>
 
